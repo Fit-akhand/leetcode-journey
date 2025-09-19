d00183808ef02b7bdd69184b15fb318140401490
Akhand Raj Singh <akhandrajsingh30@gmail.com>
2025-09-19 14:43:09 +0530

    refactor code formatting and improve readability in combination sum solution

--- LC_39.cpp
+++ LC_39.cpp
@@ -1,9 +1,11 @@
+#include <iostream>
+#include <vector>
 #include <algorithm>
+using namespace std;
+
 class Solution {
 public:
-
-    void solve(vector<int>& arr,int target,int i,vector<int>&temp,vector<vector<int>>&ans){
-        
+    void solve(vector<int>& arr, int target, int i, vector<int>& temp, vector<vector<int>>& ans) {
         if (target == 0) {
             ans.push_back(temp);
             return;
@@ -13,19 +15,39 @@ public:
             return;
         }
 
+        // include current element (i can be reused)
         temp.push_back(arr[i]);
         solve(arr, target - arr[i], i, temp, ans); 
         temp.pop_back();
 
-        
+        // exclude current element (move forward)
         solve(arr, target, i + 1, temp, ans);
-
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> temp;
-        solve(candidates,target,0,temp,ans);
+        solve(candidates, target, 0, temp, ans);
         return ans;
     }
 };
+
+// 🔹 For local testing
+int main() {
+    Solution sol;
+    vector<int> candidates = {2,3,6,7};
+    int target = 7;
+
+    vector<vector<int>> result = sol.combinationSum(candidates, target);
+
+    cout << "Combinations are:\n";
+    for (auto &comb : result) {
+        cout << "[ ";
+        for (int num : comb) {
+            cout << num << " ";
+        }
+        cout << "]\n";
+    }
+
+    return 0;
+}
