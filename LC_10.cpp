d7edfa33a30be396c70223982ff681ba96702d3c
Akhand Raj Singh <akhandrajsingh30@gmail.com>
2025-10-19 10:43:30 +0530

    implement solution to find unique triplets that sum to zero

---
+++ LC_10.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            // Skip duplicates for the first element
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
+
+                    // Skip duplicates for left and right
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                }
+                else if (sum < 0) {
+                    left++;
+                }
+                else {
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+
+    vector<vector<int>> ans = sol.threeSum(nums);
+
+    cout << "Unique triplets that sum to 0:\n";
+    for (auto triplet : ans) {
+        cout << "[ ";
+        for (int num : triplet) {
+            cout << num << " ";
+        }
+        cout << "]\n";
+    }
+
+    return 0;
+}
