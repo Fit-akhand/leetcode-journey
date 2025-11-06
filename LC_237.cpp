83c7b30137291dea83f8c677c022df45eac4413f
Akhand Raj Singh <akhandrajsingh30@gmail.com>
2025-11-06 11:12:39 +0530

    implement solution to delete a node in a linked list

---
+++ LC_237.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+// Solution class
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        ListNode* temp = node->next;
+        node->next = temp->next;
+        delete temp;
+    }
+};
+
+// Helper function to print linked list
+void printList(ListNode* head) {
+    while (head != NULL) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+// Main function (for VS Code testing)
+int main() {
+    // Create linked list: 4 -> 5 -> 1 -> 9
+    ListNode* head = new ListNode(4);
+    head->next = new ListNode(5);
+    head->next->next = new ListNode(1);
+    head->next->next->next = new ListNode(9);
+
+    cout << "Before deletion: ";
+    printList(head);
+
+    // Let's delete node with value 5
+    Solution sol;
+    sol.deleteNode(head->next); // deletes '5'
+
+    cout << "After deletion: ";
+    printList(head);
+
+    return 0;
+}
