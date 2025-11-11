f0d7546d99f563267e97b281ebef194d211e553a
Akhand Raj Singh <akhandrajsingh30@gmail.com>
2025-11-11 13:07:03 +0530

    implement solution to generate Pascal's Triangle

---
+++ LC_118.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> pascal(numRows);
+
+        for (int i = 0; i < numRows; i++) {
+            pascal[i].resize(i + 1);
+            pascal[i][0] = pascal[i][i] = 1;
+
+            for (int j = 1; j < i; j++) {
+                pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
+            }
+        }
+
+        return pascal;
+    }
+};
+
+int main() {
+    Solution sol;
+    int numRows;
+
+    cout << "Enter number of rows: ";
+    cin >> numRows;
+
+    vector<vector<int>> result = sol.generate(numRows);
+
+    cout << "\nPascal's Triangle:\n";
+    for (auto &row : result) {
+        for (auto &val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
