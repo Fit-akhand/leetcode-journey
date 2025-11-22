ba32ff9c9e661e15460f9a42e884961c65122ff9
Akhand Raj Singh <akhandrajsingh30@gmail.com>
2025-11-22 22:27:03 +0530

    add reverse function and print utility for array operations

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,16 +1,2 @@
-#include<iostream>
-using namespace std;
 
-void reverse(int arr[],int n, int m){
-    int s=m+1;
-    int e=n-1;
-    while(s<=e){
-        swap(arr[s],arr[e]);
-        s++;
-        e--;
-    }
-}
-
-void print(int arr[],int n){
-    for(int i=0 ;i<n;i++){
-        cout << arr[i] << " ";
+                if(abs(nums[i] - nums[j]) == k) {
