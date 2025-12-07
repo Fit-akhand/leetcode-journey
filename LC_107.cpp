2bd5383d66af7de01f0be7232a03a4fc4058dfdf
Akhand Raj Singh <akhandrajsingh30@gmail.com>
2025-12-07 22:52:52 +0530

    feat: add bottom-up level order traversal (LeetCode 107)
    - Implement BFS to collect level order nodes
    - Reverse collected levels to produce bottom-up order
    - Handle null root edge case

---
+++ LC_107.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> result;
+        if (root == nullptr) return result;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int size = q.size();
+            vector<int> level;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                level.push_back(node->val);
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+
+            result.push_back(level);
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
+
+int main() {
+    // Creating sample tree:
+    //       3
+    //      / \
+    //     9  20
+    //       /  \
+    //      15   7
+
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(9);
+    root->right = new TreeNode(20);
+    root->right->left = new TreeNode(15);
+    root->right->right = new TreeNode(7);
+
+    Solution sol;
+    vector<vector<int>> ans = sol.levelOrderBottom(root);
+
+    // Print result
+    for (auto &level : ans) {
+        for (int x : level) {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
