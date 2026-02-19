bfb3a8e921f42bf2f776a365f2691d9cc7d80e55
Akhand Raj Singh <akhandrajsingh30@gmail.com>
2026-02-19 22:09:45 +0530

    feat: add countBinarySubstrings function to count binary substrings in a given string

---
+++ LC_696.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+        int prev = 0;
+        int curr = 1;
+        int ans = 0;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s[i] == s[i - 1]) {
+                curr++;
+            } else {
+                ans += min(prev, curr);
+                prev = curr;
+                curr = 1;
+            }
+        }
+
+        ans += min(prev, curr);
+        return ans;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    string s;
+    cout << "Enter binary string: ";
+    cin >> s;
+
+    int result = obj.countBinarySubstrings(s);
+
+    cout << "Output: " << result << endl;
+    return 0;
+}
